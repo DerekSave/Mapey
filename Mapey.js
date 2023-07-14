93e2edb801a73a1da7e545a80314beb069feaea9
DerekSave <123384462+DerekSave@users.noreply.github.com>
2023-07-14 14:37:07 -0400

    jaj

--- Mapey.js
+++ Mapey.js
@@ -1,13 +1,10 @@
-//let map = L.map("map").setView([40.7128, -74.006], 13);
 var OSM_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-//var OSM_ATTRIB =
-//'&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';
+//var OSM_ATTRIB = &copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';
 var osmLayer = L.tileLayer(OSM_URL);
 
 var WAQI_URL =
   "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=381eeb3d963853a18901fb6120d5ca483dee3a37";
-//var WAQI_ATTR =
-//'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';
+//var WAQI_ATTR = 'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';
 var waqiLayer = L.tileLayer(WAQI_URL);
 
 //Lista de localizaciones
@@ -28,4 +25,34 @@ localizaciones.forEach(function (localizacion) {
   circle.bindPopup("Air Quality:" + location.airQuality);
 });
 
-//381eeb3d963853a18901fb6120d5ca483dee3a37
+//Importa la biblioteca axios en el entorno de JavaScript que estás utilizando. La palabra clave require se utiliza en entornos de ejecución basados en CommonJS, como Node.js, para importar módulos o bibliotecas en el código.
+const axios = require("axios");
+
+//Definir los limites geograficos para New York
+const bounds = {
+  north: 40.915255,
+  south: 40.496044,
+  east: -73.700272,
+  west: -74.255735,
+};
+
+//Realiza la solicitud HTTP para obtener las ubicaciones
+axios
+  .get("https://api.waqi.info/map/bounds/", {
+    params: {
+      latlng: "${bounds.south},${bounds.west},${bounds.north},${bounds.east}",
+      token: "381eeb3d963853a18901fb6120d5ca483dee3a37",
+    },
+  })
+  .then((response) => {
+    //Procesa las respuesta para obtener las ubicaciones
+    const locations = response.data.data;
+
+    //Imprimir las ubicaciones obtenidas en la consola
+    consola.log(locations);
+  })
+  .catch((error) => {
+    console.error("Error al obtener las ubicaciones:", error);
+  });
+
+// Token de World Air Quality Project: 81eeb3d963853a18901fb6120d5ca483dee3a37
