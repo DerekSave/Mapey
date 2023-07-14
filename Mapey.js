16a3565d4ac690de71961c3ace47874d19e47f7a
DerekSave <123384462+DerekSave@users.noreply.github.com>
2023-07-14 12:29:04 -0400

    Adding WAQI and making circles

--- Mapey.js
+++ Mapey.js
@@ -1,21 +1,31 @@
-let map = L.map("map").setView([40.7128, -74.006], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+//let map = L.map("map").setView([40.7128, -74.006], 13);
+var OSM_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+//var OSM_ATTRIB =
+//'&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';
+var osmLayer = L.tileLayer(OSM_URL);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+var WAQI_URL =
+  "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=381eeb3d963853a18901fb6120d5ca483dee3a37";
+//var WAQI_ATTR =
+//'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';
+var waqiLayer = L.tileLayer(WAQI_URL);
 
-var circle = L.circle([40.7128, -74.006], {
-  color: "green",
-  fillColor: "yellow",
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(map);
+//Lista de localizaciones
+var localizaciones = [{ lat: 40.7128, lon: -74.006, airQuality: 80 }];
 
-circle.bindPopup("I am a circle.");
+var map = L.map("map").setView([40.7128, -74.006], 13);
+map.addLayer(osmLayer).addLayer(waqiLayer);
+
+// Iterar sobre cada ubicacion y crear un circulo en el mapa
+localizaciones.forEach(function (localizacion) {
+  var circle = L.circle([localizacion.lat, localizacion.lon], {
+    color: "green",
+    fillColor: "yellow",
+    fillOpacity: 0.5,
+    radius: 500,
+  }).addTo(map);
+
+  circle.bindPopup("Air Quality:" + location.airQuality);
+});
+
+//381eeb3d963853a18901fb6120d5ca483dee3a37
