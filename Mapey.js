6d34d3fe479fe4341eed73271b2da7ca3812e800
DerekSave <123384462+DerekSave@users.noreply.github.com>
2023-07-15 05:14:51 -0400

    making merge

--- Mapey.js
+++ Mapey.js
@@ -13,45 +13,4 @@ var localizaciones = [{ lat: 40.7128, lon: -74.006, airQuality: 80 }];
 var map = L.map("map").setView([40.7128, -74.006], 13);
 map.addLayer(osmLayer).addLayer(waqiLayer);
 
-// Iterar sobre cada ubicacion y crear un circulo en el mapa
-localizaciones.forEach(function (localizacion) {
-  var circle = L.circle([localizacion.lat, localizacion.lon], {
-    color: "green",
-    fillColor: "yellow",
-    fillOpacity: 0.5,
-    radius: 500,
-  }).addTo(map);
 
-  circle.bindPopup("Air Quality:" + location.airQuality);
-});
-
-//Importa la biblioteca axios en el entorno de JavaScript que estás utilizando. La palabra clave require se utiliza en entornos de ejecución basados en CommonJS, como Node.js, para importar módulos o bibliotecas en el código.
-var axios = require("axios");
-
-//Definir los limites geograficos para New York
-const bounds = {
-  north: 40.915255,
-  south: 40.496044,
-  east: -73.700272,
-  west: -74.255735,
-};
-
-//Realiza la solicitud HTTP para obtener las ubicaciones
-get("https://api.waqi.info/map/bounds/", {
-  params: {
-    latlng: "${bounds.south},${bounds.west},${bounds.north},${bounds.east}",
-    token: "381eeb3d963853a18901fb6120d5ca483dee3a37",
-  },
-})
-  .then((response) => {
-    //Procesa las respuesta para obtener las ubicaciones
-    const locations = response.data.data;
-
-    //Imprimir las ubicaciones obtenidas en la consola
-    consola.log(locations);
-  })
-  .catch((error) => {
-    console.error("Error al obtener las ubicaciones:", error);
-  });
-
-// Token de World Air Quality Project: 81eeb3d963853a18901fb6120d5ca483dee3a37
